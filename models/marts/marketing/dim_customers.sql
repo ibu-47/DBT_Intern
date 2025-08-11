c12edee3e081842e00fccb748eba44b23b46f3e6
Syed Ibrahim <syedibrahimb2004@gmail.com>
2025-08-11 12:09:28 +0000

    Added stg_stripe__payment and fct_orders

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,31 +1,29 @@
-WITH customers AS (
-    SELECT *
-    FROM {{ ref('stg_jaffle_shop__customers') }}
-        
+with customers as (
+    select * from {{ ref ('stg_jaffle_shop__customers')}}
 ),
-orders AS (
-    SELECT *
-    FROM {{ ref('stg_jaffle_shop__orders')}}
+orders as (
+    select * from {{ ref ('fct_orders')}}
 ),
-customer_orders AS (
-    SELECT
+customer_orders as (
+    select
         customer_id,
-        MIN(order_date) AS first_order_date,
-        MAX(order_date) AS most_recent_order_date,
-        COUNT(order_id) AS number_of_orders
-    FROM orders
-    GROUP BY customer_id
-),  
-final AS (
-    SELECT
+        min (order_date) as first_order_date,
+        max (order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
+    from orders
+    group by 1
+),
+ final as (
+    select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        COALESCE(customer_orders.number_of_orders, 0) AS number_of_orders
-    FROM customers
-    LEFT JOIN customer_orders USING (customer_id)
+        coalesce (customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
+    from customers
+    left join customer_orders using (customer_id)
 )
-SELECT * 
-FROM final
+select * from final
